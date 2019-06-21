3b09f2b328c2622ca7eea2159681e0ec23f6cce3
Victoria Cushing <victoriaccushing@gmail.com>
2019-06-21 05:49:59 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,11 @@
 # Add your variables here
+
+#variables
+first_number = 10
+second_number = 5
+
+#calculations 
+sum = (first_number + second_number)
+difference = (first_number - second_number)
+product = (first_number * second_number)
+quotient = (first_number / second_number)
