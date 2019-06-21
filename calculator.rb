d20d3eec1848f7ad01b7dfe678c8f193b7329055
Victoria Cushing <victoriaccushing@gmail.com>
2019-06-21 05:47:54 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,10 @@
 # Add your variables here
 
+#variables
 first_number = 10
 second_number = 5
+
+#calculations 
 sum = (first_number + second_number)
 difference = (first_number - second_number)
+product = (first_number * second_number)
